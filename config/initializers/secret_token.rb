49a8efdd635dfbd566a926f03a61a791eedb259a
Hajer Dlame <hdlame@brotherhoodmutual.com>
2012-07-02 15:40:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '93b05d0438da359571aa4992ede25e4ea34500cf80acbe7089725d29ac1525e975895354ab51c9df91d5d0940ad5aad6a94a7c494a698d3bec60866122857075'
